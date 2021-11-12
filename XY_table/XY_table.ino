8773271ef789fab0f81374d65956e77cededafab
63n0m3 <88727337+63n0m3@users.noreply.github.com>
2021-11-12 12:16:08 +0100

    Update XY_table.ino

--- XY_table/XY_table.ino
+++ XY_table/XY_table.ino
@@ -1,7 +1,7 @@
 //This software was coded together with in deepth software explanation via a video tutorial:
 //  https://youtu.be/hIaf1pSfH0I
 //by GenOme, https://github.com/63n0m3/XYtable/
-//btc: bc1q5u49442fc08kugxp70xcer322njh3l5hplzw2k
+//btc: 1H8XwyQogdTFekH9w5CPonKFq9upmiqZ1P
 
 #include <EEPROM.h>
 #include <Adafruit_GFX.h>
